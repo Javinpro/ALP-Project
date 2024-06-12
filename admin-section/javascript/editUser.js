fbdcce1b97b137d79b5079f565f868ac87f7aa32
javinpro <javinpro12@gmail.com>
2024-06-13 00:57:50 +0800

    sudah done side bar nya dan edit user page nya

--- admin-section/javascript/editUser.js
+++ admin-section/javascript/editUser.js
@@ -1,6 +1,10 @@
-let btn = document.querySelector('#btn');
-let sidebar = document.querySelector('.sidebar');
+// Pilih elemen dengan id "toggle-btn" dan simpan dalam variabel 'hamburger'
+const hamburger = document.querySelector("#toggle-btn");
 
-btn.onclick = function () {
-    sidebar.classList.toggle('active');
-};
+// Tambahkan event listener untuk mendengarkan event "click" pada elemen 'hamburger'
+hamburger.addEventListener("click",function(){
+    // Ketika elemen 'hamburger' diklik, pilih elemen dengan id "sidebar"
+    // dan toggle kelas "expand" pada elemen tersebut
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+
